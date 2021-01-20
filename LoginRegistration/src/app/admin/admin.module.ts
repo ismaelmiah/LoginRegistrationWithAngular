466318a15842a541783940cbc8421a6945d58265
ismail5g <ismail27.dec@gmail.com>
2021-01-20 20:40:12 +0600

    User router to edit component correctly

--- LoginRegistration/src/app/admin/admin.module.ts
+++ LoginRegistration/src/app/admin/admin.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserDetailComponent, UserEditComponent, UserItemComponent, UserListComponent, UsersComponent } from './components';
+import { UserDetailComponent, UserListComponent, UsersComponent } from './components';
 import { AdminRoutingModule } from './admin.routing.module';
 
 
 
 @NgModule({
-  declarations: [UsersComponent, UserListComponent, UserDetailComponent, UserItemComponent, UserEditComponent],
+  declarations: [UsersComponent, UserListComponent, UserDetailComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
--- LoginRegistration/src/app/admin/components/users/users.component.ts
+++ LoginRegistration/src/app/admin/components/users/users.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/Model';
+import { DataService } from 'src/app/services';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
-export class UsersComponent implements OnInit {
-
-  constructor() {
-   }
-
-  ngOnInit(): void {
+export class UsersComponent implements OnInit, OnDestroy {
+  users: User[] = [];
+  dataSubscription: Subscription;
+  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router) {}
+  ngOnDestroy(): void {
+    this.dataSubscription.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.dataSubscription = this.dataService.getAll().subscribe((data) => {
+      this.users = data;
+    });
+  }
+
+  onEdit(id: number){
+    this.router.navigate(['user/edit', id])
+  }
+
+  onDelete(id: number){
+    this.router.navigate(['user/edit', id])
+  }
 }
